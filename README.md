930302d6fc2adf66f9868004225c38368fa2bb22
Hodan Ibrahim <hodanibrahim@hodans-air.home>
2023-05-12 12:19:53 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Work-Day-Scheduler
+# 05 Third-Party APIs: Work Day Scheduler
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. 
+
+
+## Github Pages
+
+Locate the deployed application here[]
+
+## Description
+This calender application will allow employees with busy schedules to add and save important events on their daily planner so that they can manage their time efficiently.
+
+
+## Used Technologies
+HTML
+CSS
+BootStrap
+JQuery
+
+
+## Screenshots
+
+
+<img width="692" alt="Screenshot 2023-05-12 at 07 53 57" src="https://github.com/Ze7Hu/worschedule/assets/123417090/3c24857c-4e44-47d2-bda8-db228104fec2">
+
+
+<img width="692" alt="Screenshot 2023-05-12 at 11 42 33" src="https://github.com/Ze7Hu/worschedule/assets/123417090/ff5442f6-e72e-4872-8269-ea1bac4b52c6">
+
+
+
---
+++ script.js
@@ -0,0 +1,43 @@
+// display today's day and date.
+var currentTime = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentTime);
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+
+  function hourUpdater() {
+    var currentHour = moment().hours();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (currentHour > blockHour) {
+        $(this).addClass("past");
+      } else if (currentHour === blockHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#hour-09 .description").val(localStorage.getItem("hour-09"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  hourUpdater();
+});
